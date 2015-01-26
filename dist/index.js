7b6242e8faf192395a5827fdd331c92a829bfd24
Dennis Sänger <mail@dennis.to>
2015-01-26 14:19:46 +0100

    chore(core): first version of juice injector

---
+++ dist/index.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var _prototypeProperties = function (child, staticProps, instanceProps) {
+  if (staticProps) Object.defineProperties(child, staticProps);
+  if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
+};
+
+var Juice = (function () {
+  function Juice() {
+    this.bindings = {};
+  }
+
+  _prototypeProperties(Juice, null, {
+    bind: {
+      value: function bind(key) {
+        return new Binding(this, key);
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    },
+    set: {
+      value: function set(key, object) {
+        this.bindings[key] = object;
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    },
+    get: {
+      value: function get(key) {
+        return this.bindings[key];
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    },
+    apply: {
+      value: function apply(fn, that) {
+        var argumentList = fn.$inject.map(this.get, this);
+        return fn.apply(that, argumentList);
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    }
+  });
+
+  return Juice;
+})();
+
+var Binding = (function () {
+  function Binding(injector, key) {
+    this.injector = injector;
+    this.key = key;
+  }
+
+  _prototypeProperties(Binding, null, {
+    to: {
+      value: function to(object) {
+        this.injector.set(this.key, object);
+        return this.injector;
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    }
+  });
+
+  return Binding;
+})();
+
+module.exports = Juice;
---
+++ index.es6
@@ -0,0 +1,37 @@
+class Juice {
+  constructor() {
+    this.bindings = {};
+  }
+
+  bind(key) {
+    return new Binding(this, key);
+  }
+
+  set(key, object) {
+    this.bindings[key] = object;
+  }
+
+  get(key) {
+    return this.bindings[key];
+  }
+
+  apply(fn, that) {
+    var argumentList = fn.$inject.map(this.get, this);
+    return fn.apply(that, argumentList);
+  }
+
+}
+
+class Binding {
+  constructor(injector, key) {
+    this.injector = injector;
+    this.key = key;
+  }
+
+  to(object) {
+    this.injector.set(this.key, object);
+    return this.injector;
+  }
+}
+
+module.exports = Juice;
