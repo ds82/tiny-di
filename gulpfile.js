1bf9160d524a50bf3c2ab36c18f358a5b554abd9
Dennis Sänger <mail@dennis.to>
2015-02-04 10:37:56 +0100

    feat(core): add sourcemap support

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@
 
 var gulp   = require('gulp');
 var to5    = require('gulp-6to5');
+var smaps  = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
 gulp.task('6to5', function() {
   return gulp.src('index.es6')
+    .pipe(smaps.init())
     .pipe(to5())
     .pipe(rename('index.js'))
+    .pipe(smaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['6to5'], function() {
   gulp.watch(['index.es6'], ['6to5']);
 });
 
