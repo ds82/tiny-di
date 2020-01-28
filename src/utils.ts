503fdbba198609709e95a76d10d15cacf7e5a9fc
Dennis <github.com@mail.ds82.de>
2020-01-28 08:25:02 +0100

    feat(utils): pipeM only returns promise if required

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,9 @@ export const reduce = reducer => start => list => list.reduce(reducer, start);
 export const map = fn => list => list.map(fn);
 
 export const pipeM = (...[fst, ...pipeline]) => (...args) =>
-  reduce((acc, fn) => acc.then(fn))(Promise.resolve(fst(...args)))(pipeline);
+  reduce((acc, fn) => (isPromise(acc) ? acc.then(fn) : fn(acc)))(
+    resolve(fst(...args))
+  )(pipeline);
 
 export const partition = fn => list => [
   list.filter(fn),
